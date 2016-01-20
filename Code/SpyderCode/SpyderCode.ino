521da172cd55fc7925b4fdc70d63693d60ca8b00
Sarah <benetto19@gmail.com>
2016-01-20 11:44:24 -0800

    Goodbye, Spyder
    RIP in peace. It was fun, my friend :)

--- Code/SpyderCode/SpyderCode.ino
+++ Code/SpyderCode/SpyderCode.ino
@@ -1,5 +1,3 @@
-
-
 #include <Wire.h>
 #include <Adafruit_PWMServoDriver.h>
 
@@ -31,56 +29,53 @@ Adafruit_PWMServoDriver pwmL = Adafruit_PWMServoDriver(0x41);
  LEFT ELBOW: INVERSE
  LEFT WRIST: DIRECT
  */
-#define R1WRISTMAX  555 //correct
+#define R1WRISTMAX  555 //r1 = correct
 #define R1WRISTMIN  200
 #define R1SHOULDERMAX  430
 #define R1SHOULDERMIN  171
 #define R1ELBOWMAX  567
 #define R1ELBOWMIN  155
 
-#define R2WRISTMAX 555 
+#define R2WRISTMAX 555 //r2 estimate
 #define R2WRISTMIN  200
 #define R2SHOULDERMAX  430
 #define R2SHOULDERMIN  171
 #define R2ELBOWMAX  567
 #define R2ELBOWMIN  155
 
-#define R3WRISTMAX  555
+#define R3WRISTMAX  555 //r3 estimate
 #define R3WRISTMIN  200
 #define R3SHOULDERMAX 430 
 #define R3SHOULDERMIN  171
 #define R3ELBOWMAX  567
 #define R3ELBOWMIN 155
 
-#define L1WRISTMAX        490 //correct
+#define L1WRISTMAX        490 //l1 = correct
 #define L1WRISTMIN        100
 #define L1SHOULDERMAX     350
 #define L1SHOULDERMIN     100
-#define L1ELBOWMAX        500//estimate
-#define L1ELBOWMIN        200//estimate
+#define L1ELBOWMAX        500
+#define L1ELBOWMIN        200
 
-#define L2WRISTMAX        490 //correct
+#define L2WRISTMAX        490 //l2 estimate
 #define L2WRISTMIN        100
 #define L2SHOULDERMAX     350
 #define L2SHOULDERMIN     100
-#define L2ELBOWMAX        500//estimate
-#define L2ELBOWMIN        200//estimate
+#define L2ELBOWMAX        500
+#define L2ELBOWMIN        200
 
-#define L3WRISTMAX        490 //correct
+#define L3WRISTMAX        490//l3 estimate
 #define L3WRISTMIN        100
 #define L3SHOULDERMAX     350
 #define L3SHOULDERMIN     100
-#define L3ELBOWMAX        500//estimate
-#define L3ELBOWMIN        200//estimate
-
-//#define SERVOMIN  150 
-//#define SERVOMAX  600 
+#define L3ELBOWMAX        500
+#define L3ELBOWMIN        200
 
 ServoInfo shouldR1(14, R1SHOULDERMIN, R1SHOULDERMAX); ServoInfo elbowR1(15, R1ELBOWMIN, R1ELBOWMAX); ServoInfo wristR1(13, R1WRISTMIN, R1WRISTMAX);
-ServoInfo shouldR2(10, R2SHOULDERMIN, R2SHOULDERMAX); ServoInfo elbowR2(11, R2ELBOWMIN, R2ELBOWMAX); ServoInfo wristR2(9, R2WRISTMIN, R2WRISTMAX);
+ServoInfo shouldR2(6, R2SHOULDERMIN, R2SHOULDERMAX); ServoInfo elbowR2(7, R2ELBOWMIN, R2ELBOWMAX); ServoInfo wristR2(5, R2WRISTMIN, R2WRISTMAX);
 ServoInfo shouldR3(2, R3SHOULDERMIN, R3SHOULDERMAX); ServoInfo elbowR3(3, R3ELBOWMIN, R3ELBOWMAX); ServoInfo wristR3(1, R3WRISTMIN, R3WRISTMAX);
 ServoInfo shouldL1(2, L1SHOULDERMIN, L1SHOULDERMAX); ServoInfo elbowL1(3, L1ELBOWMIN, L1ELBOWMAX); ServoInfo wristL1(1, L1WRISTMIN, L1WRISTMAX);
-ServoInfo shouldL2(6, L2SHOULDERMIN, L2SHOULDERMAX); ServoInfo elbowL2(7, L2ELBOWMIN, L2ELBOWMAX); ServoInfo wristL2(5, L2WRISTMIN, L2WRISTMAX);
+ServoInfo shouldL2(10, L2SHOULDERMIN, L2SHOULDERMAX); ServoInfo elbowL2(11, L2ELBOWMIN, L2ELBOWMAX); ServoInfo wristL2(9, L2WRISTMIN, L2WRISTMAX);
 ServoInfo shouldL3(14, L3SHOULDERMIN, L3SHOULDERMAX); ServoInfo elbowL3(15, L3ELBOWMIN, L3ELBOWMAX); ServoInfo wristL3(13, L3WRISTMIN, L3WRISTMAX);
 
 ServoInfo shouldersR[] = {shouldR1, shouldR2, shouldR3};
@@ -182,10 +177,10 @@ uint16_t Lconvert(double dec, ServoInfo servo) {//elbows are inverse, others are
 void drive(char side, ServoInfo servo, double pos);
 void drive(char side, ServoInfo servo, double pos) {
   if(side == 'r') {
-    pwmR.setPWM(servo.num, 1024, Rconvert(pos, servo)); 
+    pwmR.setPWM(servo.num, 0, Rconvert(pos, servo)); 
   }
   else { //side == l
-    pwmL.setPWM(servo.num, 1024, Lconvert(pos, servo));
+    pwmL.setPWM(servo.num, 0, Lconvert(pos, servo));
   }
 }
 
@@ -197,7 +192,7 @@ void prompt(String str) {
   }
 }//end void
 
-
+//servos numbered 0 through 8 from the front of the bot, from shoulder to wrist
 void oneServo(); //drives one at a time by command
 void oneServo() { 
   prompt("r/l: ");
@@ -216,16 +211,6 @@ void oneServo() {
   delay(500);
 } 
 
-void standUp() { //shoulders/elbows to 0.5, wrists to 0 
-  for(int i = 0; i < 3; i++) {
-    drive('r', shouldersR[i], 0.4); 
-    drive('l', shouldersL[i], 0.4);
-    drive('r', elbowsR[i], 0.5); 
-    drive('l', elbowsL[i], 0.5);
-    drive('r', wristsR[i], 0.7); 
-    drive('l', wristsL[i], 0.7); 
-  }
-}
 
 void setup() {
   Serial.begin(9600);
@@ -243,4 +228,5 @@ void loop() {
 oneServo();
 
 }//end loop
+
 
